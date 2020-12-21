dd3ed2f5365b59da4e9ae21966c78f19c6437661
Tim Doolan <timdoolan@Tims-MacBook-Pro.local>
2020-12-21 22:35:31 +0000

    create subscribe work flow and sagas for state

--- src/state/Auth.state.ts
+++ src/state/Auth.state.ts
@@ -1,25 +1,80 @@
-const AUTH_ACTIONS = {
-  SUBSCRIBE_FIREBASE_AUTH: 'SUBSCRIBE_FIREBASE_AUTH',
-};
+/**
+ * @name Auth state
+ * @description Sets out the actions, reducder and saga of authenticating a user
+ */
 
-interface authStateInterface {
-  initializing: boolean;
-  user: object;
-}
+import {call, put, takeLatest} from 'redux-saga/effects';
 
-const authState: authStateInterface = {
-  initializing: true,
+import AuthService from '../services/Auth';
+
+/**
+ * Actions
+ */
+export const SUBSCRIBE_FIREBASE_AUTH = 'SUBSCRIBE_FIREBASE_AUTH';
+export const SUBSCRIBE_SUCCESSFUL = 'SUBSCRIBE_SUCCESSFUL';
+export const SUBSCRIBE_FAIL = 'SUBSCRIBE_FAIL';
+
+/**
+ * Reducer
+ */
+const initialState = {
+  initialising: false,
+  loading: true,
+  errorCode: null,
+  isLoggedIn: false,
   user: null,
 };
 
-export const authentication = (state = authState, action) => {
+export const authentication = (state = initialState, action) => {
   switch (action.type) {
-    // case AUTH_ACTIONS.SUBSCRIBE_FIREBASE_AUTH: {
-    //   return {
-    //     ...authState,
-    //   };
-    // }
+    case SUBSCRIBE_FIREBASE_AUTH: {
+      return {
+        ...initialState,
+        loading: true,
+        initialising: true,
+      };
+    }
+    case SUBSCRIBE_SUCCESSFUL: {
+      return {
+        ...initialState,
+        loading: false,
+        initialising: false,
+        user: action.user,
+      };
+    }
+    case SUBSCRIBE_FAIL: {
+      return {
+        ...initialState,
+        loading: false,
+        initialising: true,
+        user: action.error,
+      };
+    }
     default:
       return state;
   }
 };
+
+/**
+ * Dispatch
+ */
+export const subscribeFireBaseAuth = () => ({
+  type: SUBSCRIBE_FIREBASE_AUTH,
+});
+
+/**
+ * Sagas
+ */
+export function* attemptSubscribe() {
+  try {
+    const user = yield call(AuthService.subscriber);
+
+    yield put({type: SUBSCRIBE_SUCCESSFUL, user});
+  } catch (error) {
+    yield put({type: SUBSCRIBE_FAIL, error: error});
+  }
+}
+
+export function* authSaga() {
+  yield takeLatest(SUBSCRIBE_FIREBASE_AUTH, attemptSubscribe);
+}
