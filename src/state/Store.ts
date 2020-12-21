843d4aa1d488e6bbd2e01286f7ff6cc02bc51217
Tim Doolan <timdoolan@Tims-MacBook-Pro.local>
2020-12-21 20:28:26 +0000

    remove console.log

--- src/state/Store.ts
+++ src/state/Store.ts
@@ -5,6 +5,4 @@ const composeEnhancers =
   (window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] as typeof compose) || compose;
 const store = createStore(reducers, /* preloadedState, */ composeEnhancers());
 
-console.log(store, 'create store');
-
 export default store;
