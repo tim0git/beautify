1bd7483d39494112830d7a7aac9427ca6b878cc3
Tim Doolan <timdoolan@Tims-MacBook-Pro.local>
2020-12-21 22:36:24 +0000

    connect sagas to redux store

--- src/state/Store.ts
+++ src/state/Store.ts
@@ -1,8 +1,19 @@
-import {createStore, compose} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import reducers from './Reducers';
+import {sagas} from './Sagas';
+
+// create the saga middleware
+const sagaMiddleware = createSagaMiddleware();
 
 const composeEnhancers =
   (window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] as typeof compose) || compose;
-const store = createStore(reducers, /* preloadedState, */ composeEnhancers());
+
+const store = createStore(
+  reducers,
+  /* preloadedState, */ composeEnhancers(applyMiddleware(sagaMiddleware)),
+);
+
+sagas.forEach(sagaMiddleware.run);
 
 export default store;
