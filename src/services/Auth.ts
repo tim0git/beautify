27273e60f720bb583b0774c7e34223b70ebaa87c
Tim Doolan <timdoolan@Tims-MacBook-Pro.local>
2020-12-21 17:18:42 +0000

    sudo Firebase Auth Services

---
+++ src/services/Auth.ts
@@ -0,0 +1,9 @@
+// Firebase Auth
+
+// as a user i should be able to create an account
+
+// as a user i should be able to Login with my phone number and OTP
+
+// as a user i should be able to log out
+
+// as a user i should not be asked to Login if my session is still active
