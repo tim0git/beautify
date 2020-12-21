01c5a8c8e6b2d52ebd72895b221ea102d094db8d
Tim Doolan <timdoolan@Tims-MacBook-Pro.local>
2020-12-21 20:26:40 +0000

    wrap root component in state provider

--- src/index.tsx
+++ src/index.tsx
@@ -7,11 +7,17 @@
  */
 
 import React from 'react';
+import {Provider} from 'react-redux';
+import store from './state/Store';
 
 import Landing_Page from './components/screens/Landing/Landing_Screen';
 
 const App: () => React$Node = () => {
-  return <Landing_Page />;
+  return (
+    <Provider store={store}>
+      <Landing_Page />
+    </Provider>
+  );
 };
 
 export default App;
