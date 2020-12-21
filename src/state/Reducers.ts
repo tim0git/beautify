3f028a9279113a4779c134b2f3de216297c63353
Tim Doolan <timdoolan@Tims-MacBook-Pro.local>
2020-12-21 20:24:58 +0000

    create redux combine reducers

---
+++ src/state/Reducers.ts
@@ -0,0 +1,10 @@
+import {combineReducers} from 'redux';
+import {authentication} from './Auth.state';
+
+const appReducer = combineReducers({authentication});
+
+const rootReducer = (state, action) => {
+  return appReducer(state, action);
+};
+
+export default rootReducer;
