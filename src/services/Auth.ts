5ce51552dd0b79a7a8232f08bd4b8f1fab050fed
Tim Doolan <timdoolan@Tims-MacBook-Pro.local>
2020-12-21 19:46:34 +0000

    create Auth class and define subscribe method

--- src/services/Auth.ts
+++ src/services/Auth.ts
@@ -1,3 +1,19 @@
+import auth from '@react-native-firebase/auth';
+export class Auth {
+  // Method
+
+  onAuthStateChanged(user) {
+    // dispatch action AUTH_STATE_CHANGE
+    // set state with user : user && initializing : false
+  }
+
+  subscriber() {
+    // dispatch action SUBSCRIBE_FIREBASE_AUTH
+    // set auth reducer to default values.
+    return auth().onAuthStateChanged(this.onAuthStateChanged);
+  }
+}
+
 // Firebase Auth
 
 // as a user i should be able to create an account
@@ -7,3 +23,9 @@
 // as a user i should be able to log out
 
 // as a user i should not be asked to Login if my session is still active
+
+// authState: {
+//   initializing: true; // set to false once firebase has connected,
+//   user: {
+//   } // user object returned form firebase??
+// }
