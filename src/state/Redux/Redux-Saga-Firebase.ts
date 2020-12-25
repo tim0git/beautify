207fbc1ce862aa947fe6dbbeb4bcf324aa0275b3
tim0git <tim.doolan88@icloud.com>
2020-12-25 20:46:09 +0000

    remove comments

--- src/state/Redux/Redux-Saga-Firebase.ts
+++ src/state/Redux/Redux-Saga-Firebase.ts
@@ -1,6 +1,6 @@
 import firebase from '@react-native-firebase/app';
 import ReduxSagaFirebase from 'redux-saga-firebase';
-import '@react-native-firebase/auth'; // <--- import it here
+import '@react-native-firebase/auth';
 // import '@react-native-firebase/firestore'; // <--- import it here
 
 const firebaseApp = firebase.apps[0];
