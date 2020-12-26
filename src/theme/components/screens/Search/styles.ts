f24f821bb99ddda9125e597f77e564c12cd8553d
tim0git <tim.doolan88@icloud.com>
2020-12-26 20:23:58 +0000

    impliment new search bar component in search screen

--- src/theme/components/screens/Search/styles.ts
+++ src/theme/components/screens/Search/styles.ts
@@ -3,8 +3,12 @@ export const styles = {
   container: {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
     alignSelf: 'center',
+  },
+  searchBarButtonContainer: {
+    flex: 1 / 3,
+    justifyContent: 'flex-end',
   },
 };
