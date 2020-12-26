bdab421f627d0594c8fac1530cf70ef6ed91c5bb
tim0git <tim.doolan88@icloud.com>
2020-12-26 12:42:11 +0000

    create stacks for tab navigation

--- src/navigator.tsx
+++ src/navigator.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import Search_Container from './containers/Search_Container';
@@ -7,16 +8,56 @@ import Bookings_Container from './containers/Bookings_Container';
 import Inbox_Container from './containers/Inbox_Container';
 import Profile_Container from './containers/Profile_Container';
 
+const SearchStack = createStackNavigator();
+
+function SearchStackScreen() {
+  return (
+    <SearchStack.Navigator>
+      <SearchStack.Screen name="Search_Landing" component={Search_Container} />
+    </SearchStack.Navigator>
+  );
+}
+
+const BookingsStack = createStackNavigator();
+
+function BookingsStackScreen() {
+  return (
+    <BookingsStack.Navigator>
+      <BookingsStack.Screen name="Bookings_Landing" component={Bookings_Container} />
+    </BookingsStack.Navigator>
+  );
+}
+
+const MessagesStack = createStackNavigator();
+
+function MessagesStackScreen() {
+  return (
+    <MessagesStack.Navigator>
+      <MessagesStack.Screen name="Inbox" component={Inbox_Container} />
+    </MessagesStack.Navigator>
+  );
+}
+
+const ProfileStack = createStackNavigator();
+
+function ProfileStackScreen() {
+  return (
+    <ProfileStack.Navigator>
+      <ProfileStack.Screen name="Profile_Landing" component={Profile_Container} />
+    </ProfileStack.Navigator>
+  );
+}
+
 const Tab = createBottomTabNavigator();
 
 export default function AppNavigationContainer() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Search" component={Search_Container} />
-        <Tab.Screen name="Bookings" component={Bookings_Container} />
-        <Tab.Screen name="Inbox" component={Inbox_Container} />
-        <Tab.Screen name="Profile" component={Profile_Container} />
+        <Tab.Screen name="Search" component={SearchStackScreen} />
+        <Tab.Screen name="Bookings" component={BookingsStackScreen} />
+        <Tab.Screen name="Inbox" component={MessagesStackScreen} />
+        <Tab.Screen name="Profile" component={ProfileStackScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
