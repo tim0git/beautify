8cd112cfe3a6044dbc8e5de1bf3e8b37a9ae5b0d
tim0git <tim.doolan88@icloud.com>
2020-12-26 19:02:35 +0000

    import button stories

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -4,12 +4,14 @@
 // https://github.com/elderfo/react-native-storybook-loader.git
 
 function loadStories() {
+  require('../src/components/atoms/Button/Button.stories');
   require('../src/components/screens/Landing/Landing_Screen.stories');
   require('../src/components/screens/Search/Search_Screen.stories');
 }
 
 const stories = [
-  '../src/components/pages/Landing/Landing_Page.stories',
+  '../src/components/atoms/Button/Button.stories',
+  '../src/components/screens/Landing/Landing_Page.stories',
   '../src/components/screens/Search/Search_Screen.stories',
 ];
 
