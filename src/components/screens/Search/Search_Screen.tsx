cef71fd1d11b8a69b7ebe055c18a7d6c8b6ef6de
tim0git <tim.doolan88@icloud.com>
2020-12-26 20:24:23 +0000

    refactor to use searchBarButton

--- src/components/screens/Search/Search_Screen.tsx
+++ src/components/screens/Search/Search_Screen.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import {SafeAreaView, View, Text, StatusBar} from 'react-native';
+import {SafeAreaView, View, Text, StatusBar, Button} from 'react-native';
 import {ThemeProvider} from '../../../services/ThemeProvider';
+import SearchBarButton from '../../molecules/SearchBarButton/SearchBarButton';
 
-export interface Props {}
+export interface Props {
+  navigation: {
+    navigate: (path: string) => void;
+  };
+}
 
-const Search_Screen: React.FC<Props> = ({}) => {
+const Search_Screen: React.FC<Props> = (props) => {
   const {styles} = ThemeProvider('Search');
   const {barStyle} = ThemeProvider('global').styles;
 
@@ -13,7 +18,22 @@ const Search_Screen: React.FC<Props> = ({}) => {
       <StatusBar barStyle={barStyle} />
       <SafeAreaView>
         <View style={styles.container}>
-          <Text>Search Screen</Text>
+          <View style={styles.searchBarButtonContainer}>
+            <SearchBarButton
+              title="Search for a treatment or provider"
+              type="default"
+              textAlign="center"
+              accessible={true}
+              accessibilityLabel="TEST A11Y LABEL"
+              accessibilityHint="TEST A11Y HINT"
+              accessibilityRole="button"
+              disabled={false}
+              testID="TEST_ID"
+              onPress={() => {
+                props.navigation.navigate('Test Path');
+              }}
+            />
+          </View>
         </View>
       </SafeAreaView>
     </>
