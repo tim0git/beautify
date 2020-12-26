38d58f8798b074fe0b9ab1344cbc2f3233b5bcc1
tim0git <tim.doolan88@icloud.com>
2020-12-26 18:09:59 +0000

    create search screen story

---
+++ src/components/screens/Search/Search_Screen.stories.js
@@ -0,0 +1,5 @@
+import {storiesOf} from '@storybook/react-native';
+import React from 'react';
+import Search_Screen from './Search_Screen';
+
+storiesOf('Search_Screen', module).add('Default', () => <Search_Screen />);
