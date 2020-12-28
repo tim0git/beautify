82a002d7edb6394d82ae39571389852522fafd72
tim0git <tim.doolan88@icloud.com>
2020-12-28 15:09:40 +0000

    run storybook

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -5,23 +5,25 @@
 
 function loadStories() {
   require('../src/components/atoms/Button/Button.stories');
-  require('../src/components/atoms/Title/Title.stories');
+  require('../src/components/atoms/Icon/IconMCI.stories');
   require('../src/components/atoms/SubTitle/SubTitle.stories');
+  require('../src/components/atoms/Title/Title.stories');
   require('../src/components/molecules/Screen_Instructions/Screen_Instructions.stories');
   require('../src/components/molecules/Search_Bar/Search_Bar.stories');
-  require('../src/components/screens/Landing/Landing_Screen.stories');
   require('../src/components/screens/Home/Home_Screen.stories');
+  require('../src/components/screens/Landing/Landing_Screen.stories');
   require('../src/components/screens/Search/Search_Screen.stories');
 }
 
 const stories = [
   '../src/components/atoms/Button/Button.stories',
-  '../src/components/atoms/Title/Title.stories',
+  '../src/components/atoms/Icon/IconMCI.stories',
   '../src/components/atoms/SubTitle/SubTitle.stories',
-  '../src/components/molecules/Search_Bar/Search_Bar.stories',
+  '../src/components/atoms/Title/Title.stories',
   '../src/components/molecules/Screen_Instructions/Screen_Instructions.stories',
-  '../src/components/screens/Landing/Landing_Page.stories',
+  '../src/components/molecules/Search_Bar/Search_Bar.stories',
   '../src/components/screens/Home/Home_Screen.stories',
+  '../src/components/screens/Landing/Landing_Screen.stories',
   '../src/components/screens/Search/Search_Screen.stories',
 ];
 
