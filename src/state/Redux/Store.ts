4740fdb8166cfc93022686fadd3d73ba79af81b2
tim0git <tim.doolan88@icloud.com>
2020-12-28 15:02:55 +0000

    lint & prettify store

--- src/state/Redux/Store.ts
+++ src/state/Redux/Store.ts
@@ -6,13 +6,9 @@ import {sagas} from './Sagas';
 // create the saga middleware
 const sagaMiddleware = createSagaMiddleware();
 
-const composeEnhancers =
-  (window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] as typeof compose) || compose;
+const composeEnhancers = (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ as typeof compose) || compose;
 
-const store = createStore(
-  reducers,
-  /* preloadedState, */ composeEnhancers(applyMiddleware(sagaMiddleware)),
-);
+const store = createStore(reducers, /* preloadedState, */ composeEnhancers(applyMiddleware(sagaMiddleware)));
 
 sagas.forEach(sagaMiddleware.run);
 
