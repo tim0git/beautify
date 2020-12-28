4e7b75a2969c4ab3664ef1979ac1b95af776bfbf
tim0git <tim.doolan88@icloud.com>
2020-12-28 21:28:52 +0000

    impliment JSDom to render in tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,12 +1,40 @@
 import 'jest-enzyme';
 import 'react-native-gesture-handler/jestSetup';
-
-// setup file
 import {configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+/**
+ * Setup
+ */
+
+const {JSDOM} = require('jsdom');
+const {document} = new JSDOM('', {
+  url: 'https://cdl.co.uk', // or whatever
+}).window;
+
+global.document = document;
+global.window = document.defaultView;
+Object.keys(document.defaultView).forEach((property) => {
+  if (typeof global[property] === 'undefined') {
+    global[property] = document.defaultView[property];
+  }
+});
+
+function suppressDomErrors() {
+  const suppressedErrors = /(React does not recognize the.*prop on a DOM element|Unknown event handler property|is using uppercase HTML|Received `true` for a non-boolean attribute `accessible`|The tag.*is unrecognized in this browser|PascalCase)/;
+  // eslint-disable-next-line no-console
+  console.error = (message) => {
+    if (message.match(suppressedErrors)) {
+    }
+  };
+}
+suppressDomErrors();
+
 configure({adapter: new Adapter()});
 
+/**
+ * Setup
+ */
 // Mock reanimated in order to test navigation
 jest.mock('react-native-reanimated', () => {
   const Reanimated = require('react-native-reanimated/mock');
