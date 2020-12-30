f7519ac9d141d7bfac76ad07366b7aeb1ee7ee91
tim0git <tim.doolan88@icloud.com>
2020-12-30 21:49:57 +0000

    cretae client and wrap app in ApolloProvider

--- src/index.tsx
+++ src/index.tsx
@@ -9,11 +9,21 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {Provider} from 'react-redux';
 import store from './state/Redux/Store';
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+});
 
 import AppNavigationContainer from './navigator';
 
 const App = (): React.ReactElement => {
-  return <Provider store={store}>{AppNavigationContainer()}</Provider>;
+  return (
+    <Provider store={store}>
+      <ApolloProvider client={client}>{AppNavigationContainer()}</ApolloProvider>
+    </Provider>
+  );
 };
 
 export default App;
