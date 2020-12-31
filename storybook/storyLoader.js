abc4620597cb39c80b1ba8ac7b2c691b20f3b2a7
tim0git <tim.doolan88@icloud.com>
2020-12-31 16:35:38 +0000

    story loader auto added Text stories

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -7,6 +7,7 @@ function loadStories() {
   require('../src/components/atoms/Button/Button.stories');
   require('../src/components/atoms/Icon/Icon.stories');
   require('../src/components/atoms/SubTitle/SubTitle.stories');
+  require('../src/components/atoms/Text/Text.stories');
   require('../src/components/atoms/Title/Title.stories');
   require('../src/components/molecules/Screen_Instructions/Screen_Instructions.stories');
   require('../src/components/molecules/Search_Bar/Search_Bar.stories');
@@ -19,6 +20,7 @@ const stories = [
   '../src/components/atoms/Button/Button.stories',
   '../src/components/atoms/Icon/Icon.stories',
   '../src/components/atoms/SubTitle/SubTitle.stories',
+  '../src/components/atoms/Text/Text.stories',
   '../src/components/atoms/Title/Title.stories',
   '../src/components/molecules/Screen_Instructions/Screen_Instructions.stories',
   '../src/components/molecules/Search_Bar/Search_Bar.stories',
