c3d0efd1293b5ed0c92fe2660f56d85998ea1f58
tim0git <tim.doolan88@icloud.com>
2021-01-02 19:22:34 +0000

    temp render Menu_List in profile screen to check appearance

--- src/components/screens/Profile/Profile_Screen.tsx
+++ src/components/screens/Profile/Profile_Screen.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {SafeAreaView, View, StatusBar} from 'react-native';
 import {ThemeProvider} from '../../../services/ThemeProvider';
 import Header from '../../molecules/Header/Header';
+import Menu_List from '../../molecules/Menu_List/Menu_List';
 import Guest_Welcome from '../../organisms/Guest_Welcome/Guest_Welcome';
 export interface Props {
   navigation: {
@@ -22,6 +23,9 @@ const Profile_Screen: React.FC<Props> = ({navigation}) => {
         <View style={styles.container}>
           <View style={styles.guestWelcomeContainer}>
             <Guest_Welcome screenInstructionsProps={screenInstructionsProps} navigation={navigation} />
+          </View>
+          <View style={styles.menuListContainer}>
+            <Menu_List />
           </View>
         </View>
       </SafeAreaView>
