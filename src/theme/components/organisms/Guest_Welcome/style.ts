c8f1b5d7a8321abd03c8ee6f3b078c91de38a73c
tim0git <tim.doolan88@icloud.com>
2021-01-02 19:57:58 +0000

    add padding to GuestWelcome to space in Profile Screen

--- src/theme/components/organisms/Guest_Welcome/style.ts
+++ src/theme/components/organisms/Guest_Welcome/style.ts
@@ -3,6 +3,7 @@ import {size} from '../../../global/styles';
 export const style = {
   container: {
     paddingHorizontal: size.padding.xxlg,
+    paddingBottom: size.padding.xlg,
   },
   screenInstructionsContainer: {},
   loginClusterContainer: {
