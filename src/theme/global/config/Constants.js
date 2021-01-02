cf287797202f06317925a00b1dc17b7db39e6427
tim0git <tim.doolan88@icloud.com>
2021-01-02 18:08:21 +0000

    decouple button text and icon description using global enum

---
+++ src/theme/global/config/Constants.js
@@ -0,0 +1,19 @@
+export const PROFILE_MENU_BUTTONS = {
+  About: 'About Beautify',
+  Help: 'Help Centre',
+  Legal: 'Legal Stuff',
+  FutureBookings: 'Upcoming Bookings',
+  PastBookings: 'Previous Bookings',
+  Contact: 'Contact Details',
+  Payment: 'Payment Methods',
+  Marketing: 'Marketing Preferences',
+  Notifications: 'Notifications',
+};
+
+export const PROFILE_GUEST_MENU = {
+  General: {
+    [PROFILE_MENU_BUTTONS.About]: {},
+    [PROFILE_MENU_BUTTONS.Help]: {},
+    [PROFILE_MENU_BUTTONS.Legal]: {},
+  },
+};
