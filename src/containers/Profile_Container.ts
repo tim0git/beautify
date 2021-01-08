8d707a512b4fa52d5441c9cc8d2ae745abab2a10
tim0git <tim.doolan88@icloud.com>
2021-01-08 19:43:52 +0000

    map sign out to props

--- src/containers/Profile_Container.ts
+++ src/containers/Profile_Container.ts
@@ -1,9 +1,18 @@
 import {connect} from 'react-redux';
-
+import {bindActionCreators, Dispatch} from 'redux';
 import Profile_Screen from '../components/screens/Profile/Profile_Screen';
+import {signOut} from '../state/Auth.state';
 
-const mapStateToProps = (state) => ({});
+const mapStateToProps = (state) => ({
+  isLoggedIn: state.authentication.isLoggedIn,
+});
 
-const mapDispatchToProps = {};
+const mapDispatchToProps = (dispatch: Dispatch) =>
+  bindActionCreators(
+    {
+      signOut: signOut,
+    },
+    dispatch,
+  );
 
 export default connect(mapStateToProps, mapDispatchToProps)(Profile_Screen);
