c51c18cfa6ebe55251f4da8cedc5d0d735c7225b
tim0git <tim.doolan88@icloud.com>
2021-01-08 19:48:19 +0000

    create basic guest / logged in props

--- src/components/screens/Profile/Profile_Screen.stories.js
+++ src/components/screens/Profile/Profile_Screen.stories.js
@@ -2,4 +2,15 @@ import {storiesOf} from '@storybook/react-native';
 import React from 'react';
 import Profile_Screen from './Profile_Screen';
 
-storiesOf('Design System/Screens/Profile_Screen', module).add('Profile', () => <Profile_Screen />);
+const guestProps = {
+  isLoggedIn: false,
+};
+
+const loggedInProps = {
+  isLoggedIn: true,
+  signOut: () => {}, // redux auth dispatch
+};
+
+storiesOf('Design System/Screens/Profile_Screen', module)
+  .add('Profile_Guest', () => <Profile_Screen {...guestProps} />)
+  .add('Profile_LoggedIn', () => <Profile_Screen {...loggedInProps} />);
