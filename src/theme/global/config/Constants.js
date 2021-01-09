445ace5452664eb91fa79f65c222dea9b2aa6d6d
tim0git <tim.doolan88@icloud.com>
2021-01-09 21:52:33 +0000

    install get random values to support uuid

--- src/theme/global/config/Constants.js
+++ src/theme/global/config/Constants.js
@@ -1,4 +1,5 @@
-const {v4: uuidv4} = require('uuid');
+import 'react-native-get-random-values'; // provides UUID access the global object crypto.getRandomValues
+import {v4 as uuidv4} from 'uuid';
 
 export const PROFILE_MENU_BUTTONS = {
   About: 'About Beautify',
