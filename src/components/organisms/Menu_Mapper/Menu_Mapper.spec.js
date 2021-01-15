7a3964e298d42e874458fd4b6627eb0f256f0c8c
tim0git <tim.doolan88@icloud.com>
2021-01-15 21:41:55 +0000

    map Notifications Menu

--- src/components/organisms/Menu_Mapper/Menu_Mapper.spec.js
+++ src/components/organisms/Menu_Mapper/Menu_Mapper.spec.js
@@ -3,7 +3,7 @@ import 'react-native';
 import 'jest-enzyme';
 import {shallow} from 'enzyme';
 import Menu_Mapper from './Menu_Mapper';
-import {USER_PROFILE_MENU, PROFILE_GUEST_MENU} from '../../../theme/global/config/Constants';
+import {USER_PROFILE_MENU, PROFILE_GUEST_MENU, NOTIFICATIONS_MENU} from '../../../theme/global/config/Constants';
 
 const guestProps = {
   navigation: {navigate: () => {}},
@@ -13,6 +13,10 @@ const guestProps = {
 const userProps = {
   navigation: {navigate: () => {}},
   userProfileMenu: USER_PROFILE_MENU,
+};
+
+const bookingProps = {
+  userProfileMenu: NOTIFICATIONS_MENU,
 };
 
 describe('<Menu_Mapper />', () => {
@@ -26,6 +30,13 @@ describe('<Menu_Mapper />', () => {
       const wrapper = shallow(<Menu_Mapper {...userProps} />);
       const profileUserMenu = wrapper.findWhere((node) => node.prop('testID') === 'Profile_User_Menu');
       expect(profileUserMenu).toExist();
+    });
+    test('should render notificatons bookings menu', () => {
+      const wrapper = shallow(<Menu_Mapper {...bookingProps} />);
+      const notificatonsBookingsMenu = wrapper.findWhere(
+        (node) => node.prop('testID') === 'Notificatons_Bookings_Menu',
+      );
+      expect(notificatonsBookingsMenu).toExist();
     });
   });
 });
