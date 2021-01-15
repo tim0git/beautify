2882cf967370ab389a2017809ab2bc46aa8fa8a4
tim0git <tim.doolan88@icloud.com>
2021-01-15 21:42:10 +0000

    add stories for menu mapper

--- src/components/organisms/Menu_Mapper/Menu_Mapper.stories.js
+++ src/components/organisms/Menu_Mapper/Menu_Mapper.stories.js
@@ -2,13 +2,24 @@ import {storiesOf} from '@storybook/react-native';
 import React from 'react';
 import CenterView from '../../../../storybook/stories/CenterView';
 import Menu_Mapper from './Menu_Mapper';
-import {USER_PROFILE_MENU} from '../../../theme/global/config';
+import {USER_PROFILE_MENU, NOTIFICATIONS_MENU, PROFILE_GUEST_MENU} from '../../../theme/global/config';
 
 const defaultProps = {
   userProfileMenu: USER_PROFILE_MENU,
   navigation: () => {},
 };
 
+const guestProps = {
+  navigation: () => {},
+  userProfileMenu: [PROFILE_GUEST_MENU],
+};
+
+const bookingProps = {
+  userProfileMenu: NOTIFICATIONS_MENU,
+};
+
 storiesOf('Design System/Organism/Menu_Mapper', module)
   .addDecorator((getStory) => <CenterView>{getStory()}</CenterView>)
-  .add('Menu_Mapper_Default', () => <Menu_Mapper {...defaultProps} />);
+  .add('Menu_Mapper_Guest_Profile', () => <Menu_Mapper {...guestProps} />)
+  .add('Menu_Mapper_User_Profile', () => <Menu_Mapper {...defaultProps} />)
+  .add('Menu_Mapper_Bookings', () => <Menu_Mapper {...bookingProps} />);
