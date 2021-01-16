108059f021811f2450941c5c6d14f63ee48b060a
tim0git <tim.doolan88@icloud.com>
2021-01-16 22:23:54 +0000

    add notifications screen to design system

--- src/components/screens/Notification_Settings/Notification_Settings.stories.js
+++ src/components/screens/Notification_Settings/Notification_Settings.stories.js
@@ -5,6 +5,6 @@ import Notification_Settings_Screen from './Notification_Settings_Screen';
 
 const defaultProps = {};
 
-storiesOf('Notification_Settings', module)
+storiesOf('Design System/Screens/Notification_Settings', module)
   .addDecorator((getStory) => <CenterView>{getStory()}</CenterView>)
   .add('Notification_Settings_Default', () => <Notification_Settings_Screen {...defaultProps} />);
