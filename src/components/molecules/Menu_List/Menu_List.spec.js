33186a8730d73948393b807a43ee96cb597ed66a
tim0git <tim.doolan88@icloud.com>
2021-01-16 22:33:48 +0000

    fix lint

--- src/components/molecules/Menu_List/Menu_List.spec.js
+++ src/components/molecules/Menu_List/Menu_List.spec.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-shadow */
 import React from 'react';
 import 'react-native';
 import 'jest-enzyme';
--- src/components/screens/Notification_Settings/Notification_Settings.spec.js
+++ src/components/screens/Notification_Settings/Notification_Settings.spec.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import 'react-native';
 import 'jest-enzyme';
-import {shallow, render, mount} from 'enzyme';
+import {shallow} from 'enzyme';
 import Notification_Settings_Screen from './Notification_Settings_Screen';
+
+const defaultProps = {};
 
 describe('<Notification_Settings />', () => {
   describe('<Render>', () => {
-    test('should render a notification settings component', () => {});
+    test('should render a notification settings component', () => {
+      const wrapper = shallow(<Notification_Settings_Screen {...defaultProps} />);
+      const notificationSettings = wrapper.findWhere((node) => node.prop('testID') === 'Notification_Settings_Screen');
+      expect(notificationSettings).toExist();
+    });
   });
 });
