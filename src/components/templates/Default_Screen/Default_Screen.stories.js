a71a1e5dbbc0c6bca59503eaa3750bf322b78b9e
tim0git <tim.doolan88@icloud.com>
2021-01-18 13:52:31 +0000

    add default screen template to design system

--- src/components/templates/Default_Screen/Default_Screen.stories.js
+++ src/components/templates/Default_Screen/Default_Screen.stories.js
@@ -2,16 +2,15 @@ import {storiesOf} from '@storybook/react-native';
 import React from 'react';
 import CenterView from '../../../../storybook/stories/CenterView';
 import Default_Screen_Template from './Default_Screen_Template';
+import {Text} from 'react-native';
 
 const defaultProps = {
   screenTitle: 'Default Screen Template',
-  render: () => {
-    null;
-  },
+  render: <Text>Test jsx</Text>,
   backButton: true,
   testID: 'Default_Screen_Template_Story',
 };
 
-storiesOf('Default_Screen', module)
+storiesOf('Design System/Templates/Default_Screen', module)
   .addDecorator((getStory) => <CenterView>{getStory()}</CenterView>)
   .add('Default_Screen_Back_Button', () => <Default_Screen_Template {...defaultProps} />);
