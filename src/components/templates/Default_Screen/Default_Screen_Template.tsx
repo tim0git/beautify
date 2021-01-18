5a5efef92451cb4246b89d91b6c42ee14973d1ee
tim0git <tim.doolan88@icloud.com>
2021-01-18 10:15:12 +0000

    refactor Profile to use default screen template

--- src/components/templates/Default_Screen/Default_Screen_Template.tsx
+++ src/components/templates/Default_Screen/Default_Screen_Template.tsx
@@ -10,7 +10,7 @@ import {ThemeProvider} from '../../../services/ThemeProvider';
 
 export interface Props {
   screenTitle: string;
-  render: () => any;
+  render: any;
   backButton: boolean;
   testID: string;
 }
