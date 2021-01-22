d9181ac7754898cc7c2e31c67275899f3d5367b2
tim0git <tim.doolan88@icloud.com>
2021-01-22 09:07:33 +0000

    re name story

--- src/components/screens/Home/Home_Screen.stories.js
+++ src/components/screens/Home/Home_Screen.stories.js
@@ -2,4 +2,4 @@ import {storiesOf} from '@storybook/react-native';
 import React from 'react';
 import Home_Screen from './Home_Screen';
 
-storiesOf('Design System/Screens/Home_Screen', module).add('No Bookings', () => <Home_Screen />);
+storiesOf('Design System/Screens/Home_Screen', module).add('Home_No_Bookings', () => <Home_Screen />);
