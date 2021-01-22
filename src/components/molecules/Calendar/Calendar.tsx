5d24ee28814a18383446a87f62a4a4d6f804fa6d
tim0git <tim.doolan88@icloud.com>
2021-01-22 14:07:27 +0000

    remove used imports

--- src/components/molecules/Calendar/Calendar.tsx
+++ src/components/molecules/Calendar/Calendar.tsx
@@ -4,16 +4,14 @@
  *
  */
 import React, {useState} from 'react';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
 import {ThemeProvider} from '../../../services/ThemeProvider';
 import {Calendar as CalendarWix} from 'react-native-calendars';
-import {theme} from '@storybook/react-native/dist/preview/components/Shared/theme';
 import IconRN from '../../atoms/Icon/Icon';
-// import moment from 'moment';
 
 export interface Props {}
 const Calendar: React.FC<Props> = ({}) => {
-  const {config, dict, style} = ThemeProvider('Calendar');
+  const {config, style} = ThemeProvider('Calendar');
   const [selectedDay, setSelectedDay] = useState({});
 
   const {calendarProps, iconProps} = config;
@@ -32,7 +30,6 @@ const Calendar: React.FC<Props> = ({}) => {
         onMonthChange={(month) => {
           console.log('month changed', month);
         }}
-        // // Replace default arrows with custom ones (direction can be 'left' or 'right')
         renderArrow={(direction) => <IconRN {...iconProps[direction]} color={style.arrowColor} />}
         onPressArrowLeft={(subtractMonth) => subtractMonth()}
         onPressArrowRight={(addMonth) => addMonth()}
