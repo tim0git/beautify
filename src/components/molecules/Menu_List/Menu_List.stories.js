056d321b24bc885982c603fe861c219e8e41daa1
tim0git <tim.doolan88@icloud.com>
2021-01-30 23:02:48 +0000

    add Legal Stuff to Menu List Stories

--- src/components/molecules/Menu_List/Menu_List.stories.js
+++ src/components/molecules/Menu_List/Menu_List.stories.js
@@ -6,6 +6,7 @@ import {
   PROFILE_GUEST_MENU,
   NOTIFICATIONS_BOOKINGS_MENU,
   NOTIFICATIONS_MENU_BUTTONS,
+  LEGAL_STUFF_GENERAL_MENU,
 } from '../../../theme/global/config';
 
 const profileGuestProps = {
@@ -32,7 +33,17 @@ const notificationMenuProps = {
   testID: 'TEST_MENU_LIST_NOTIFICATIONS_BOOKINGS',
 };
 
+const legalStuffMenuProps = {
+  navigation: {
+    navigate: () => {},
+  },
+  DATA: LEGAL_STUFF_GENERAL_MENU.DATA,
+  headerText: LEGAL_STUFF_GENERAL_MENU.headerText,
+  testID: 'TEST_LEGAL_STUFF_GENERAL_MENU',
+};
+
 storiesOf('Design System/Molecule/Menu_List', module)
   .addDecorator((getStory) => <CenterView>{getStory()}</CenterView>)
   .add('Menu_List_Profile_Guest', () => <Menu_List {...profileGuestProps} />)
-  .add('Menu_List_Notification_Menu', () => <Menu_List {...notificationMenuProps} />);
+  .add('Menu_List_Notification_Menu', () => <Menu_List {...notificationMenuProps} />)
+  .add('Menu_List_Legal_Stuff', () => <Menu_List {...legalStuffMenuProps} />);
