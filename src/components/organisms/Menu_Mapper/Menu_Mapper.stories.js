83e719075fa840af8912c993150e2162786c10d0
tim0git <tim.doolan88@icloud.com>
2021-01-30 23:15:03 +0000

    add Legal Stuff Menu to Menu Mapper stories

--- src/components/organisms/Menu_Mapper/Menu_Mapper.stories.js
+++ src/components/organisms/Menu_Mapper/Menu_Mapper.stories.js
@@ -9,6 +9,7 @@ import {
   NOTIFICATIONS_MENU_BUTTONS,
   MARKETING_MENU,
   MARKETING_MENU_BUTTONS,
+  LEGAL_STUFF_MENU,
 } from '../../../theme/global/config';
 
 const defaultProps = {
@@ -44,9 +45,15 @@ const marketingProps = {
   },
 };
 
+const legalStuffProps = {
+  menuData: LEGAL_STUFF_MENU,
+  navigation: () => {},
+};
+
 storiesOf('Design System/Organism/Menu_Mapper', module)
   .addDecorator((getStory) => <CenterView>{getStory()}</CenterView>)
   .add('Menu_Mapper_Guest_Profile', () => <Menu_Mapper {...guestProps} />)
   .add('Menu_Mapper_User_Profile', () => <Menu_Mapper {...defaultProps} />)
   .add('Menu_Mapper_Bookings', () => <Menu_Mapper {...bookingProps} />)
-  .add('Menu_Mapper_Marketing', () => <Menu_Mapper {...marketingProps} />);
+  .add('Menu_Mapper_Marketing', () => <Menu_Mapper {...marketingProps} />)
+  .add('Menu_Mapper_Legal_Stuff', () => <Menu_Mapper {...legalStuffProps} />);
