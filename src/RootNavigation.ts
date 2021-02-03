5654a7ff3397aad021dc1d897b23319746331e3a
tim0git <tim.doolan88@icloud.com>
2021-02-03 16:42:45 +0000

    export navigation ref from root navigation

--- src/RootNavigation.ts
+++ src/RootNavigation.ts
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import {NavigationContainerRef} from '@react-navigation/native';
 
-const navigationRef = React.createRef<NavigationContainerRef>();
+export const navigationRef = React.createRef<NavigationContainerRef>();
 
 export function navigate(name: string, params?: object): void {
   navigationRef.current?.navigate(name, params);
