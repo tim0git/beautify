261141835ec227b59150a30baf4df8f52b22aa86
tim0git <tim.doolan88@icloud.com>
2021-02-07 13:35:41 +0000

    prepare import of fcm

--- src/state/Redux/Redux-Saga-Firebase.ts
+++ src/state/Redux/Redux-Saga-Firebase.ts
@@ -1,9 +1,10 @@
 import firebase from '@react-native-firebase/app';
 import ReduxSagaFirebase from 'redux-saga-firebase';
 import '@react-native-firebase/auth';
-// import '@react-native-firebase/firestore'; // <--- import it here
+// import '@react-native-firebase/messaging'; // <--- import it here
 
 const firebaseApp = firebase.apps[0];
+// @ts-ignore
 const rsf = new ReduxSagaFirebase(firebaseApp);
 
 export default rsf;
