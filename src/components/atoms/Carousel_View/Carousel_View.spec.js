e5bd6bc1d63f6f6bfe6b5afc0ce8f629885eb2fa
tim0git <tim.doolan88@icloud.com>
2021-02-11 18:22:31 +0000

    working build roll back deps to last know good config

--- src/components/atoms/Carousel_View/Carousel_View.spec.js
+++ src/components/atoms/Carousel_View/Carousel_View.spec.js
@@ -1,7 +1,9 @@
 // import React from 'react';
-// import 'react-native';
-// import 'jest-enzyme';
+import 'react-native';
+import 'jest-enzyme';
 // import {shallow, render, mount} from 'enzyme';
 // import Carousel_View from './Carousel_View';
 
-// describe('<Carousel_View />', () => {});
+describe('<Carousel_View />', () => {
+  test.todo('should render a carousel component');
+});
