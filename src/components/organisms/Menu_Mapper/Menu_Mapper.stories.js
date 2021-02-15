3ac87cbfc927cc8345f0c7a5c49eafbe9601f4ba
tim0git <tim.doolan88@icloud.com>
2021-02-15 16:04:17 +0000

    remove center view

--- src/components/organisms/Menu_Mapper/Menu_Mapper.stories.js
+++ src/components/organisms/Menu_Mapper/Menu_Mapper.stories.js
@@ -1,6 +1,5 @@
 import {storiesOf} from '@storybook/react-native';
 import React from 'react';
-import CenterView from '../../../../storybook/stories/CenterView';
 import Menu_Mapper from './Menu_Mapper';
 import {
   USER_PROFILE_MENU,
@@ -51,7 +50,6 @@ const legalStuffProps = {
 };
 
 storiesOf('Design System/Organism/Menu_Mapper', module)
-  .addDecorator((getStory) => <CenterView>{getStory()}</CenterView>)
   .add('Menu_Mapper_Guest_Profile', () => <Menu_Mapper {...guestProps} />)
   .add('Menu_Mapper_User_Profile', () => <Menu_Mapper {...defaultProps} />)
   .add('Menu_Mapper_Bookings', () => <Menu_Mapper {...bookingProps} />)
