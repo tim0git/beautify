7100fe7b2f5cec610c41138e907c881524cd5cf9
tim0git <tim.doolan88@icloud.com>
2021-02-18 15:29:39 +0000

    impliment env var mock of firebase auth

--- src/state/Redux/Redux-Saga-Firebase.ts
+++ src/state/Redux/Redux-Saga-Firebase.ts
@@ -1,8 +1,11 @@
+import {MOCK_FIREBASE} from '@env';
 import firebase from '@react-native-firebase/app';
 import ReduxSagaFirebase from 'redux-saga-firebase';
 import '@react-native-firebase/auth';
 // import '@react-native-firebase/messaging'; // <--- import it here
 
+firebase.auth().settings.appVerificationDisabledForTesting = MOCK_FIREBASE ? true : false;
+console.log(firebase.auth().settings.appVerificationDisabledForTesting);
 const firebaseApp = firebase.apps[0];
 // @ts-ignore
 const rsf = new ReduxSagaFirebase(firebaseApp);
