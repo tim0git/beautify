618db5819f5779be598f12e2db6c9d47f9770f83
tim0git <tim.doolan88@icloud.com>
2021-02-19 12:58:30 +0000

    test getUserIdToken

--- src/state/Firebase.helpers.ts
+++ src/state/Firebase.helpers.ts
@@ -1,4 +1,8 @@
 async function getUserIDToken(user) {
+  if (!user) {
+    throw new Error(`A valid user object must be passed as an argument, user is ${user}`);
+  }
+
   if (!user.user) {
     return '';
   }
